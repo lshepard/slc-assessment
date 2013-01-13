1558df9fa4ee4a41884e66e82d910ca657093506
Luke Shepard <luke@esparklearning.com>
2013-01-12 18:23:15 -0600

    Default site created by rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlcAssessment::Application.config.secret_token = 'baef4aad9988924754751d61d9ff585f298a4ac7e771176f28b2132c67688e537c3822d87756643f02bfaaa4ac12e6a90e1fd8618925f2c51960200ba751e96f'
